2d06c0ae5ca317af39d89ef85b14816a72e00228
Shadrackssien <shadrack6960@gmail.com>
2024-04-12 01:27:19 +0000

    Colors and Price Components

--- src/Sidebar/Colors/Colors.js
+++ src/Sidebar/Colors/Colors.js
@@ -2,7 +2,36 @@ import "./Colors.css"
 
 function Colors() {
   return (
-    <div>Colors</div>
+    <div className="ttt">
+      <h2 className="sidebar-title colors-title">Colors</h2>
+
+      <label className="sidebar-label-container">
+        <input type="radio" name="test3" />
+        <span className="checkmark"></span>All
+      </label>
+      <label className="sidebar-label-container">
+        <input type="radio" name="test3" />
+        <span className="checkmark"></span>Black
+      </label>
+      <label className="sidebar-label-container">
+        <input type="radio" name="test3" />
+        <span className="checkmark"></span>Blue
+      </label>
+      <label className="sidebar-label-container">
+        <input type="radio" name="test3" />
+        <span className="checkmark"></span>Red
+      </label>
+      <label className="sidebar-label-container">
+        <input type="radio" name="test3" />
+        <span className="checkmark"></span>Green
+      </label>
+      <label className="sidebar-label-container">
+        <input type="radio" name="test3" />
+        <span className="checkmark"></span>White
+      </label>
+
+
+    </div>
   )
 }
 
